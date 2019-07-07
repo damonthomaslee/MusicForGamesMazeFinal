e0fe32820da2f3f9335206b490e56be252c59286
damonthomaslee <lee@hfm.eu>
2019-07-07 10:56:25 +0200

    GeometryEstabledOnNewParentObject
    Beacon sound, onTriggerEnter added, space-bar click, using "accelerated mixing"

---
+++ A-Maze-in/Assets/1MusicForGames2/triggerAudio.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SteamAudio;
+using UnityEngine;
+
+public class triggerAudio : MonoBehaviour
+{
+    AudioSource sound;
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+  
+    } 
+
+    void OnTriggerEnter()
+    {
+        sound.Play ();
+
+    }
+    void OnTriggerExit()
+    {
+        sound.Stop ();
+
+    }
+}
+
