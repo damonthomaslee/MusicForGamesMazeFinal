25f73e63a2d0c22e355fd78de9167dc8563917df
Anselm <anselm.weber@gmail.com>
2019-07-24 19:24:36 +0200

    Changed Gameplay
    Now the music stays the same, but the player is equipped with an allmighty detector which is going faster if the end is near.

--- A-Maze-in/Assets/GettingFaster.cs
+++ A-Maze-in/Assets/GettingFaster.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class GettingFaster : MonoBehaviour
 {
+    public int scale;
+    public float distanceNow;
+    public AudioSource AudioSource;
+    //public float pitch;
+
+    private float startDistance;
+    private GameObject Goal;
+
+
+    private float DistanceToPitch()
+    {
+        float pitch;
+        float distance;
+
+
+        distance = Vector3.Distance(Goal.transform.position, transform.position) / scale;
+        pitch = 1 + (startDistance - distance) * (1 / startDistance);
+
+        return pitch;
+    }
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Goal = GameObject.Find("Goal");
+
+        scale = 1;
+        startDistance = Vector3.Distance(Goal.transform.position, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceNow = Vector3.Distance(Goal.transform.position, transform.position);
+
+        AudioSource.pitch = DistanceToPitch();
+
+        if (Input.GetKey("k") && AudioSource.isPlaying == false)
+        {
+            AudioSource.Play();
+        }
+
+        if (Input.GetKeyUp("k"))
+        {
+            AudioSource.Stop();
+        }
+
     }
+
 }
